a33572db3e8735edd7268e3ae549ee44af1a2b49
Fatma OK <okfatma7@gmail.com>
2019-07-12 22:07:32 +0300

    eslint integration

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({ name: 'Fatma', surname: "Can", age: 26 });
+router.get('/', function(req, res) {
+	res.json({ name: 'Fatma', surname: 'Can', age: 26 });
 });
 
 module.exports = router;
